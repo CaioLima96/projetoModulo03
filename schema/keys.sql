4c150ecc7b65b125db6b83163a69dc1cf8a19afd
Joyce Lusia <joycep.lusia@gmail.com>
2021-10-21 10:44:03 -0300

    atualização keys

--- schema/keys.sql
+++ schema/keys.sql
@@ -6,22 +6,33 @@ ALTER TABLE colors
 ALTER TABLE inventories
   ADD PRIMARY KEY (id);
   
-ALTER TABLE themes
-  ADD PRIMARY KEY (id);
-  
-ALTER TABLE part_categories
-  ADD PRIMARY KEY (id);part_cat_id
-  SELECT * FROM PARTS;
 ALTER TABLE parts
-MODIFY part_num INT,
-  ADD PRIMARY KEY (part_num);
+ADD PRIMARY KEY (part_num);
+
+ALTER TABLE sets 
+ADD PRIMARY KEY(set_num);
+  
+  ALTER  TABLE part_categories
+  ADD PRIMARY KEY(id);
   
 #Queries chaves estrangeiras
 
 ALTER TABLE inventory_sets ADD FOREIGN KEY (inventory_id) REFERENCES inventories(id);
+
+ALTER TABLE inventory_sets ADD FOREIGN KEY(set_num) REFERENCES sets(set_num); #erro
+
+ALTER TABLE inventories ADD FOREIGN KEY(set_num) REFERENCES sets(set_num);#erro
 
 ALTER TABLE inventory_parts ADD FOREIGN KEY (inventory_id) REFERENCES inventories(id);
 
 ALTER TABLE parts ADD FOREIGN KEY (part_cat_id) REFERENCES part_categories(id);
 
 ALTER TABLE sets ADD FOREIGN KEY (theme_id) REFERENCES themes(id);
+
+ALTER TABLE inventory_parts ADD FOREIGN KEY (color_id) REFERENCES colors(id);
+
+ALTER TABLE inventory_parts ADD FOREIGN KEY (part_num) REFERENCES parts(part_num);
+
+ALTER TABLE parts ADD FOREIGN KEY (part_cat_id) REFERENCES part_categories(id);
+
+ALTER TABLE inventory_parts ADD FOREIGN KEY (part_num) REFERENCES parts (part_num);
