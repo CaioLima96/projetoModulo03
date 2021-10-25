31d03c481b08547a8adfe8b1f094a5256283a328
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-10-25 16:33:56 -0300

    adicionado mais prints dos resultados das queries e mais consultas ao themes

--- queries/consulta/part-consultas.sql
+++ queries/consulta/part-consultas.sql
@@ -1,4 +1,5 @@
-# Qual o rank das categorias de partes que mais se repetem em parts? -- VER PQ NÃO FOI O ORDER BY
-select pc.name as 'Categoria', count(p.part_cat_id) as 'Total'
+# Qual o rank das categorias de partes que mais se repetem em parts?
+select pc.name as 'Categoria_partes', count(p.part_cat_id) as 'Total'
 from parts as p inner join part_categories as pc
-on p.part_cat_id = pc.id group by id limit 0,20;
+on p.part_cat_id = pc.id 
+group by id order by total desc limit 0,20;
--- queries/consulta/themes-consultas.sql
+++ queries/consulta/themes-consultas.sql
@@ -4,7 +4,18 @@ from themes as th left join sets as st
 on th.id = st.theme_id limit 0,20 ;
 
 # Qual tema não possui set?
- select th.name as tema_nome, st.name as set_nome 
- from themes as th left join sets as st 
- on th.id = st.theme_id 
- where st.theme_id is null limit 0,20;
+select th.name as tema_nome, st.name as set_nome 
+from themes as th left join sets as st 
+on th.id = st.theme_id 
+where st.theme_id is null limit 0,20;
+
+# Quantos sets não possuem não possuem?
+select sum(st.theme_id is null) as 'Total' 
+from themes as th left join sets as st 
+on th.id = st.theme_id;
+
+# Quais os temas mais usados? -- nao junta todas as colunas
+select th.name as 'Nome_tema', count(st.theme_id) as 'Total' 
+from themes as th inner join sets as st 
+on th.id = st.theme_id
+group by id order by Total desc limit 0,20;
