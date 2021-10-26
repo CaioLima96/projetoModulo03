08fda999592d90f395b769b75e06bee1cda86225
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-10-26 10:52:56 -0300

    organizado os prints da queries e revisão de algumas queries

--- queries/consulta/themes-consultas.sql
+++ queries/consulta/themes-consultas.sql
@@ -9,13 +9,13 @@ from themes as th left join sets as st
 on th.id = st.theme_id 
 where st.theme_id is null limit 0,20;
 
-# Quantos sets não possuem não possuem?
+# Quantos sets não possuem não possuem? - tirar?
 select sum(st.theme_id is null) as 'Total' 
 from themes as th left join sets as st 
 on th.id = st.theme_id;
 
-# Quais os temas mais usados? -- nao junta todas as colunas
+# Quais os temas mais usados?
 select th.name as 'Nome_tema', count(st.theme_id) as 'Total' 
 from themes as th inner join sets as st 
-on th.id = st.theme_id
+on th.id = st.theme_id 
 group by id order by Total desc limit 0,20;
